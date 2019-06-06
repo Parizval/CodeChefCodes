1663497fb4b5843f8f6e26d12fe9099c3a0c1a39
Parizval <anmolgoyal@gmail.com>
2019-06-06 17:20:23 +0530

    FLOW Series

---
+++ Python/FLOW002.py
@@ -0,0 +1,3 @@
+for i in range(int(input())):
+    a,b = map(int,input().split())
+    print(a%b)
---
+++ Python/FLOW006.py
@@ -0,0 +1,6 @@
+for i in range(int(input())):
+    value = input()
+    ans = 0 
+    for b in value:
+        ans += int(b)
+    print(ans)
