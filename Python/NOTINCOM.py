ee5d640c66770893fb82b3657583946a5a444cc3
Parizval <anmolgoyal@gmail.com>
2019-06-20 00:05:03 +0530

    Pratice Questions

--- Python/NOTINCOM.py
+++ Python/NOTINCOM.py
@@ -1,9 +1,5 @@
 for a in range(int(input())):
     n,m = map(int,input().split())
-    alice = list(map(int,input().split()))
-    bertha = list(map(int,input().split()))
-    value = 0 
-    for i in range(n):
-        if alice[i] in bertha : 
-            value += 1 
-    print(value)
+    one = set(map(int,input().split()))
+    two = set(map(int,input().split()))
+    print(len(one.intersection(two)))
