b37b8468fa4b38b58acbe3dcf53571ff60012ab1
Anmol Goyal <anmolgoyal@gmail.com>
2020-01-23 14:53:06 +0530

    solved MXCH problem

---
+++ Python/Beginner/MXCH.py
@@ -0,0 +1,7 @@
+for a in range(int(input())):
+    n,k = map(int,input().split())
+    for i in range(n-k,n+1,1):
+        print(i,end=" ")
+    for i in range(1,n-k):
+        print(i,end=" ")
+    print()
